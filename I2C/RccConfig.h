2ad2b88b3faa4aa748d49fa22809836e59a16d1c
camsi1 <camsi1@secil9.siame.univ-tlse3.fr>
2024-02-05 11:11:58 +0100

    oui

---
+++ I2C/RccConfig.h
@@ -0,0 +1,32 @@
+/**
+  ******************************************************************************
+
+  CLOCK Setup For STM32F446RE
+  Author:   ControllersTech
+  Updated:  2nd Aug 2020
+
+  ******************************************************************************
+  Copyright (C) 2017 ControllersTech.com
+
+  This is a free software under the GNU license, you can redistribute it and/or modify it under the terms
+  of the GNU General Public License version 3 as published by the Free Software Foundation.
+  This software library is shared with public for educational purposes, without WARRANTY and Author is not liable for any damages caused directly
+  or indirectly by this software, read more about this on the GNU General Public License.
+
+  ******************************************************************************
+*/
+
+#include <stdlib.h> 
+#include <tinyprintf.h>
+#include <stm32f4/rcc.h>
+#include <stm32f4/gpio.h>
+#include <stm32f4/nvic.h>
+#include <stm32f4/exti.h>
+#include <stm32f4/syscfg.h>
+#include <stm32f4/tim.h>
+#include <stm32f4/adc.h>
+#include <stm32f4/dac.h>
+#include <stm32f4/i2c.h>
+
+void SysClockConfig (void);
+
