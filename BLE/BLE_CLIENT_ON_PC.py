86ddf41305db32fcd013a610baa86549a889fb5c
Lansalot <clara.lansalot34@gmail.com>
2024-02-21 18:19:53 +0100

    ESP32 PC code édité

--- BLE/BLE_CLIENT_ON_PC.py
+++ BLE/BLE_CLIENT_ON_PC.py
@@ -5,24 +5,35 @@ import argparse
 import asyncio
 from bleak import BleakScanner
 from bleak import BleakClient
-UUID = '19b10000-e8f2-537e-4f6c-d104768a1214'
+UUIDRx = '2ce4b984-2934-4e3a-abe5-1034ac6ca3f1'
+UUIDTx = '2ce4b985-2934-4e3a-abe5-1034ac6ca3f1'
 async def main():
     print("Searching Arduino Nano ESP32 'LED' device, please wait...")
     # Scan BLE devices for timeout seconds and return discovered devices with advertising data
-    devices = await BleakScanner.discover(timeout=30,
+    devices = await BleakScanner.discover(timeout=10,
                                           return_adv=True)
     for ble_device, adv_data in devices.values():
         if ble_device.name == 'LED':
             print("Device found")
+
             # Connect to Arduino Nano ESP 32 device
             async with BleakClient(ble_device.address) as client:
+                
                 print("Connected to device")
                 while True:
-                    value=10111
-                    val = await client.write_gatt_char(UUID, value)
+                    try :
+                        data=await client.read_gatt_char(UUIDRx)
+                        print("Received: {0}".format("".join(map(chr, data))))                    
+                        print("try")
+                    except Exception as e:
+                        print(f"Error:{e}")
+                        await asyncio.sleep(1.0)
+                    write_value = b"0110"
+                    await client.write_gatt_char(UUIDTx, write_value)
+                    #await asyncio.sleep(1.0)
+                    data=await client.read_gatt_char(UUIDTx)
+                    print("Received2: {0}".format("".join(map(chr, data))))
                     await asyncio.sleep(1.0)
-                    data=await client.read_gatt_char(UUID)
-                    print("Received: {0}".format("".join(map(chr, data))))
-                    await asyncio.sleep(1.0)
+                       
 if __name__ == "__main__":
     asyncio.run(main())
