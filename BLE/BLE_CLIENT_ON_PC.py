da44a0f3dfd16a8393da849214916701d6eb99ec
Lansalot <clara.lansalot34@gmail.com>
2024-02-26 17:14:51 +0100

    server esp32

--- BLE/BLE_CLIENT_ON_PC.py
+++ BLE/BLE_CLIENT_ON_PC.py
@@ -7,12 +7,106 @@ from bleak import BleakScanner
 from bleak import BleakClient
 UUIDRx = '2ce4b984-2934-4e3a-abe5-1034ac6ca3f1'
 UUIDTx = '2ce4b985-2934-4e3a-abe5-1034ac6ca3f1'
+
+findRed = 0
+findGreen = 0
+findBlue = 0
+
+def couleur(robot):
+    global findRed
+    global findGreen
+    global findBlue
+    if (robot[:2]=="00"):
+        match robot[-3:] : #robot n°1
+            case "100" : 
+                if(findRed==0) :
+                    findRed=1
+                    resp=b'000'#au robot n°1 : stop
+                else :
+                    findRed=0
+                    resp=b'011'#au robot n°2 : bouger
+                print("rouge trouvé")
+            case "010" : 
+                if(findGreen==0) :
+                    findGreen=1
+                    resp=b'000'#au robot n°1 : stop
+                else :
+                    findGreen=0
+                    resp=b'011'#au robot n°2 : bouger
+                print("vert trouvé")
+            case "001" :
+                if(findBlue==0) :
+                    findBlue=1
+                    resp=b'000'#au robot n°1 : stop
+                else :
+                    findBlue=0
+                    resp=b'011'#au robot n°2 : bouger
+                print("bleu trouvé")
+    else :
+        match robot[-3:] : #robot n°2
+            case "100" : 
+                if(findRed==0) :
+                    findRed=1
+                    resp=b'010'#au robot n°2 : stop
+                else :
+                    findRed=0
+                    resp=b'001'#au robot n°1 : bouger
+                print("rouge trouvé")
+            case "010" : 
+                if(findGreen==0) :
+                    findGreen=1
+                    resp=b'010'#au robot n°2 : stop
+                else :
+                    findGreen=0
+                    resp=b'001'#au robot n°1 : bouger
+                print("vert trouvé")
+            case "001" :
+                if(findBlue==0) :
+                    findBlue=1
+                    resp=b'010'#au robot n°2 : stop
+                else :
+                    findBlue=0
+                    resp=b'001'#au robot n°1 : bouger
+                print("bleu trouvé")
+    print(resp)
+    print(findRed,findGreen,findBlue)
+    return resp
+
+
+def response(data):
+    # trois premiers : print(data[:3])
+    # trois derniers : data[-3:]
+    #au milieu : data[2:5]
+
+    resp=b'10000'
+    match data[:2] :#check quel robot
+        case "00" :#Si robot n°1
+            match data[2:5]:
+                case "001" : #croisement
+                    resp=b'1111' #A VOIR
+                case "010" :#obstacle
+                    resp=b'008'#demi-tour
+                case "011" : #couleur
+                    resp=couleur(data)
+
+        case "01" :#Si robot n°2
+            match data[2:5]:
+                case "001" : #croisement
+                    resp=b'111'
+                case "010" :#obstacle
+                    resp=b'018'#demi-tour
+                case "011" : #couleur
+                    resp=couleur(data)
+
+    return resp
+
 async def main():
     print("Searching Arduino Nano ESP32 'LED' device, please wait...")
     # Scan BLE devices for timeout seconds and return discovered devices with advertising data
-    devices = await BleakScanner.discover(timeout=10,
+    devices = await BleakScanner.discover(timeout=20,
                                           return_adv=True)
     for ble_device, adv_data in devices.values():
+        print(ble_device)
         if ble_device.name == 'LED':
             print("Device found")
 
@@ -22,13 +116,17 @@ async def main():
                 print("Connected to device")
                 while True:
                     try :
+                        print("oui")
                         data=await client.read_gatt_char(UUIDRx)
                         print("Received: {0}".format("".join(map(chr, data))))                    
-                        print("try")
+                        print(format("".join(map(chr, data))))
+                        write_value=response(format("".join(map(chr, data))))
+                        print(write_value)
                     except Exception as e:
                         print(f"Error:{e}")
                         await asyncio.sleep(1.0)
-                    write_value = b"0110"
+                        #write_value=b'0000'
+                    #write_value = b"0110"
                     await client.write_gatt_char(UUIDTx, write_value)
                     #await asyncio.sleep(1.0)
                     data=await client.read_gatt_char(UUIDTx)
--- BLE/BLE_SERVER.ino
+++ BLE/BLE_SERVER.ino
@@ -4,14 +4,17 @@
 // https://tutoduino.fr/
 // Copyleft 2023
 #include <ArduinoBLE.h>
-BLEService Service("19b10000-e8f2-537e-4f6c-d104768a1214"); // Bluetooth® Low Energy LED Service
+#include<Wire.h>
+BLEService Service("2ce4b983-2934-4e3a-abe5-1034ac6ca3f1"); // Bluetooth® Low Energy LED Service
 // Bluetooth® Low Energy LED Switch Characteristic - custom 128-bit UUID, read and writable by central
-BLECharacteristic switchCharacteristic("19b10000-e8f2-537e-4f6c-d104768a1214", BLERead | BLEWrite,5);
-//const int ledPin = LED_BUILTIN; // internal LED pin
+BLECharacteristic writeChar("2ce4b984-2934-4e3a-abe5-1034ac6ca3f1", BLERead | BLEWrite,10);
+BLECharacteristic readChar("2ce4b985-2934-4e3a-abe5-1034ac6ca3f1", BLERead | BLEWrite,10);
+
+
 void setup() {
+  Wire.begin(0x33);
   Serial.begin(9600);
-  // set LED pin to output mode
-  //pinMode(ledPin, OUTPUT);
+
   // BLE initialization
   if (!BLE.begin()) {
     Serial.println("starting Bluetooth® Low Energy module failed!");
@@ -21,18 +24,50 @@ void setup() {
   BLE.setLocalName("LED");
   BLE.setAdvertisedService(Service);
   // add the characteristic to the service
-  Service.addCharacteristic(switchCharacteristic);
+  Service.addCharacteristic(writeChar);
+  Service.addCharacteristic(readChar);
   // add service
   BLE.addService(Service);
   // set the initial value for the characeristic:
-  switchCharacteristic.writeValue("couco");
+  writeChar.writeValue("110");
+  readChar.writeValue("100");
   // start advertising
   BLE.advertise();
   Serial.println("BLE LED Peripheral");
 }
+
+void receiveEvent(int howMany)
+{
+  String data="00";//Robot 0
+  int remainder =0;
+  while(0 < Wire.available()) // loop through all but the last
+  {
+    data = data + Wire.read();
+  }
+  Serial.println(data); 
+  // Set the characteristic's value to be the array of bytes that is actually a string.
+  writeChar.writeValue(data.c_str(),data.length());
+  
+}
+
+
+void requestEvent()
+{
+  int length = 4;
+  byte value[length + 1];  // one byte more, to save the '\0' character!
+  readChar.readValue(value, length);
+  value[length] = '\0';  // make sure to null-terminate!
+  Serial.print("Characteristic event, written: ");
+  Serial.println((char *) value);
+
+  Wire.write((char *)value);
+}
+
 void loop() {
   // wait for a Bluetooth® Low Energy central
+  //Serial.println("connecting");
   BLEDevice central = BLE.central();
+  //Serial.println("connecting");
   // check if a central is connected to this peripheral
   if (central) {
     Serial.print("Connected to central: ");
@@ -40,16 +75,27 @@ void loop() {
     Serial.println(central.address());
     // while the central is still connected to peripheral:
     while (central.connected()) {
-      // if the remote device wrote to the characteristic,
-      // use the value to control the LED:
-      if (switchCharacteristic.written()) {
-        byte dataReceived= 0;
-        switchCharacteristic.readValue(dataReceived);
-        Serial.println(dataReceived);
-        switchCharacteristic.writeValue("gg");
-      }
-    }
+      //sans I2C
+
+      String dataToSend = "00010";
+      writeChar.writeValue(dataToSend.c_str(), dataToSend.length());
+      
+      int length = 4;
+      byte value[length + 1];  // one byte more, to save the '\0' character!
+      readChar.readValue(value, length);
+      value[length] = '\0';  // make sure to null-terminate!
+      Serial.print("Characteristic event, written: ");
+      Serial.println((char *) value);
+
+      delay(1000);
+      
+      //AVEC I2C
+      //Wire.onReceive(receiveEvent);
+      //Wire.onRequest(requestEvent);
+    
+  }
     // the central has disconnected
     Serial.println("Disconnected from central: ");
   }
+  
 }
